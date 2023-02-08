ca71576473dbbbe6176b12288ea5d04e4975b710
ITSE1329 <124047516+ITSE1329@users.noreply.github.com>
2023-02-08 11:22:05 -0600

    Update Error.kt

--- Unit01/Error.kt
+++ Unit01/Error.kt
@@ -1,6 +1,6 @@
 fun main() {
     val width = 5
-    val heigh = 5
-    val area = width * heigh
-    println(area) //10
+    val height = 5
+    val area = width * height
+    println(area) //25
 }
