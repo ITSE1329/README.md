c275372b2c4016ee17d32ec5b2a0e8e25830e646
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-24 09:53:19 -0600

    made changes to files

---
+++ src/main/kotlin/Unite02/varSamples.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    var name="Azami"
+    var num=14
+    var numLetter="12"
+
+    println(name)
+    println(num)
+    println(numLetter)
+
+    println("your number times 2 is" +num*2)
+    println("Ther other number times 2 is"+(num-1)*2)
+}
