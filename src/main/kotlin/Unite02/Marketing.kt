79cbe12b12fe5f2dc1d6b0e6ef8a183e3960d23c
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-24 06:46:19 -0600

    made changes to files

---
+++ src/main/kotlin/Unite02/Marketing.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+    val friendsCount = 500 // check friends count greater than 500
+    // Atleast 500 friends = extroverts
+    // less than 500 friends = introverts
+    // less than 500 friends = introverts
+    if (friendsCount >= 500) {
+        // less than 500 friends = introverts
+        println("Adopt a dog today!")
+    } else // less than 500 friends = introverts
+    {
+        println("Buy Cat food at 20% Off")
+    }
+}
+
+
