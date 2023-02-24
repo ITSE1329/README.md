3f6ef271ca640666298ffd7981e0b2b132857f26
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-24 05:59:21 -0600

    made changes to files

---
+++ src/main/kotlin/Unite02/Variable.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    val myAge = "32"
+    var averageAge = "32"
+    averageAge = "30"
+    val testNumber = 31
+    var evenodd = testNumber % 2
+    var answer = 0
+    answer = (0 + 1 + 10) * 10 shr 3.also { answer = it }
+
+    println(myAge)
+    println(averageAge)
+    println(message = averageAge)
+    println(testNumber)
+    println(evenodd)
+    println(answer)
+    println(answer)
+
+}
