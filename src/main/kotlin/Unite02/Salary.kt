471fa766b661e8edd98cb826b4a0bc8aca7b1141
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-24 10:21:51 -0600

    made changes to files

---
+++ src/main/kotlin/Unite02/Salary.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name: String = "Elnaz Azami"
+    val age: Int = 32
+    val annualPay: Double = 750000.0
+    println("My name is $name, my age is $age and I plan to earn $$annualPay per year.")
+}
+
+
+
+
