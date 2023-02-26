aa5adbc2fcb064b73f8f972487a6d8107e88fe16
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-25 23:52:13 -0600

    made changes to files

---
+++ src/main/kotlin/Unite02/salesPrediction.kt
@@ -0,0 +1,11 @@
+
+fun main() {
+
+    val totalSales = 4600000.0 //4.6 million in total sales.
+    val salesPercent = 0.62 // 62% of total sales
+    val result: Double
+    result = totalSales * salesPercent
+    println("When the company generates$$totalSales")
+    println("in sales this year, the East Coast")
+    println("division will genrate $$result")
+}
