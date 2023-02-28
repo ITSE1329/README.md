baaf00eee641664ea8fd8ee5149cb95bbf67119f
ITSE1329 <e3337728@student.dcccd.edu>
2023-02-28 06:46:31 -0600

    made changes to files

--- src/main/kotlin/Unite02/StringMani.kt
+++ src/main/kotlin/Unite02/StringMani.kt
@@ -1,10 +1,8 @@
-import java.util.*
-
 fun main() {
 
-    val fullname = "Elnaz Azami"
-    val assignment = "Sring Manipulator"
-    val date ="02/27/2023"
+    val fullname: String = "Elnaz Azami"
+    val assignment: String = "Sring Mnaipulator"
+    val date: String ="02/27/2023"
     val cityName = "Paris"
 
     println(fullname)
@@ -12,7 +10,7 @@ fun main() {
     println(date)
     println(cityName)
     println("Number of characters in the city name: ${cityName.length}")
-    println("City name in all uppercase: ${cityName.uppercase(Locale.getDefault())}")
-    println("City name in all lowercase: ${cityName.lowercase(Locale.getDefault())}")
+    println("City name in all uppercase: ${cityName.toUpperCase()}")
+    println("City name in all lowercase: ${cityName.toLowerCase()}")
     println("First character of the city name: ${cityName.first()}")
 }
