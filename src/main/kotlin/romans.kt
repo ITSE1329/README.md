e6f2a85fabf7e08d0ef2f236977d083ebcbcef08
ITSE1329 <e3337728@student.dcccd.edu>
2023-03-01 04:45:52 -0600

    made changes to files

---
+++ src/main/kotlin/romans.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val num = 1
+    when (num) {
+        1 -> println("I")
+        2 -> println("II")
+        3 -> println("III")
+        4 -> println("IV")
+        5 -> println("V")
+        6 -> println("VI")
+        7 -> println("VII")
+        8 -> println("VIII")
+        9 -> println("IX")
+        10 -> println("X")
+        else -> println("Invalid Number")
+    }
+}
