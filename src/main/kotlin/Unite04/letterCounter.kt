f528fc4e362788ccd8508ce47063a0900fdbfb61
ITSE1329 <e3337728@student.dcccd.edu>
2023-03-16 11:58:05 -0500

    made changes to files

--- src/main/kotlin/Unite04/letterCounter.kt
+++ src/main/kotlin/Unite04/letterCounter.kt
@@ -1,17 +1,16 @@
+fun main() {
 
-    fun main() {
-
-        val input: String
-        val c: Char
-        var i = 0
-        var count = 0
-        print("Enter a string: ")
-        input = "salaria"
-        print("Enter the character to count: ")
-        'a'.also { c = it }
-        while (i < input.length) {
-            if (input[i] == c) count++
-            i++
-        }
-        println("$c occurs $count times")
+    val input: String
+    val c: Char
+    var i = 0
+    var count = 0
+    print("Enter a string: ")
+    input = "salaria"
+    print("Enter the character to count: ")
+    'a'.also { c = it }
+    while (i < input.length) {
+        if (input[i] == c) count++
+        i++
     }
+    println("$c occurs $count times")
+}
